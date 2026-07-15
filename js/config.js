7becb20e46dc7b457478e3f5311b90dee79c986e
relisa0301-hub <relisa0301@gmail.com>
2026-07-15 16:54:36 +0700

    Add configuration constants for app settings

--- js/config.js
+++ js/config.js
@@ -1,1 +1,11 @@
+const CONFIG = {
 
+    APP_NAME: "SIGAP RANI",
+
+    VERSION: "4.0",
+
+    API_URL: "https://script.google.com/macros/s/AKfycby5TYVlbTQOoE3hIYrkM5xRpLbW7DatqvKYUrk9mCIBovc_UYg1y1QxiV-PZHt6lmk/exec",
+
+    SESSION_KEY: "SIGAP_RANI_LOGIN"
+
+};
