3a1d78f7085c95f66ae70fe11a3726cadf7cdf3f
relisa0301-hub <relisa0301@gmail.com>
2026-07-15 16:54:53 +0700

    Add async postAPI function for API requests

--- js/api.js
+++ js/api.js
@@ -1,1 +1,13 @@
+async function postAPI(data){
 
+    const response = await fetch(CONFIG.API_URL,{
+
+        method:"POST",
+
+        body:JSON.stringify(data)
+
+    });
+
+    return await response.json();
+
+}
