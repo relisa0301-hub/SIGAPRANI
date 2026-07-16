b81232181353e222112731cb27c26da219789e99
relisa0301-hub <relisa0301@gmail.com>
2026-07-16 20:31:35 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
 
     APP_NAME: "SIGAP RANI",
 
-    VERSION: "4.0",
+    VERSION: "4.1",
 
     API_URL: "https://script.google.com/macros/s/AKfycbzxA6B9iTRoo0Vpz6ZP9_StKZT_QX3vFUfy9gAYfVGgxtdFJr5WGXEFGiGYDbymMqgr/exec",
 
