afad63f30c4e0e1b41b5ef443b016ad9458c585e
relisa0301-hub <relisa0301@gmail.com>
2026-07-17 18:24:34 +0700

    Refactor loadLaporan function for better clarity

--- js/laporan.js
+++ js/laporan.js
@@ -1,58 +1,112 @@
-async function loadLaporan() {
+async function loadLaporan(){
 
     const tbody = document.querySelector("#tblLaporan tbody");
 
     tbody.innerHTML = `
         <tr>
-            <td colspan="7" style="text-align:center">
+            <td colspan="8" align="center">
                 Memuat data...
             </td>
         </tr>
     `;
 
-    try {
+    try{
 
-        const res = await postAPI({
-            action: "laporan"
+        const hasil = await postAPI({
+            action:"laporan"
         });
 
-        if (!res.status) {
+        if(!hasil.status){
 
             tbody.innerHTML = `
                 <tr>
-                    <td colspan="7">Data tidak ditemukan</td>
+                    <td colspan="8" align="center">
+                        Belum ada data.
+                    </td>
                 </tr>
             `;
 
             return;
+
         }
 
-        tbody.innerHTML = "";
+        tbody.innerHTML="";
 
-        res.data.forEach((d, i) => {
+        let hadir=0;
+        let tidak=0;
 
-            tbody.innerHTML += `
-                <tr>
-                    <td>${i + 1}</td>
-                    <td>${d.tanggal}</td>
-                    <td>${d.nama}</td>
-                    <td>${d.kelas}</td>
-                    <td>${d.mapel}</td>
-                    <td>${d.guru}</td>
-                    <td>${d.status}</td>
-                </tr>
+        hasil.data.forEach((d,index)=>{
+
+            if(d.status=="Hadir"){
+                hadir++;
+            }else{
+                tidak++;
+            }
+
+            tbody.innerHTML+=`
+
+            <tr>
+
+                <td>${index+1}</td>
+
+                <td>${d.tanggal}</td>
+
+                <td>${d.jam}</td>
+
+                <td>${d.nama}</td>
+
+                <td>${d.kelas}</td>
+
+                <td>${d.mapel}</td>
+
+                <td>${d.guru}</td>
+
+                <td>${d.status}</td>
+
+            </tr>
+
             `;
 
         });
 
-    } catch (e) {
+        document.getElementById("totalData").innerHTML=hasil.data.length;
+        document.getElementById("hadir").innerHTML=hadir;
+        document.getElementById("tidak").innerHTML=tidak;
 
-        tbody.innerHTML = `
+        let persen=0;
+
+        if(hasil.data.length>0){
+
+            persen=Math.round((hadir/hasil.data.length)*100);
+
+        }
+
+        document.getElementById("persen").innerHTML=persen+"%";
+
+    }catch(err){
+
+        console.log(err);
+
+        tbody.innerHTML=`
             <tr>
-                <td colspan="7">Gagal mengambil data.</td>
+                <td colspan="8" align="center">
+                    Gagal mengambil data.
+                </td>
             </tr>
         `;
 
     }
+
+}
+
+function exportPDF(){
+
+    window.print();
+
+}
+
+function exportExcel(){
+
+    alert("Export Excel akan kita aktifkan pada revisi berikutnya.");
 
 }
